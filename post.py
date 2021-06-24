765739efc4b7f33d5803191c0f50d0931a2ce488
BlueSkyXN <xienan0714@gmail.com>
2021-06-24 09:41:25 +0800

    Create post.py

---
+++ post.py
@@ -0,0 +1,12 @@
+import requests
+url = "https://api.cybree.com/v1/instances"
+cookies = ""
+headers = {
+    "Cookie" : cookies,
+    "Content-Type" : "application/json",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+}
+
+data = '{"name":"jp01","region":"tyo1","size":"xs-1vcpu-1gb","image":"centos-7-x64","password":"UXE4MTU1Mjg4NjM="}'
+respond = requests.post(url, data= data, headers = headers)
+print(respond.json())
