20892c5a2538fa2f882867613617430ccb2f6da6
BlueSkyXN <xienan0714@gmail.com>
2021-07-11 14:07:19 +0800

    Update post.py

--- post.py
+++ post.py
@@ -7,6 +7,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
 }
 
-data = '{"name":"jp01","region":"tyo1","size":"xs-1vcpu-1gb","image":"centos-7-x64","password":"UXE4MTU1Mjg4NjM="}'
+data = '{"name":"jp01","region":"tyo1","size":"xs-1vcpu-1gb","image":"centos-7-x64","password":"QWIxMjMONTY30A=="}'
 respond = requests.post(url, data= data, headers = headers)
 print(respond.json())
